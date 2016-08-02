be50fd8bc286313ddb04b9aea203702797fc93c0
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2016-08-02 12:07:06 -0500

    Changed to generate correct filters

--- gen_coef.m
+++ gen_coef.m
@@ -1,9 +1,9 @@
-clc;
-clear all;
-sig = 'db10';
-load db10;
-S=db10;
+% This file generates the low pass and high
+% pass filter coefficients for the DWT
+wavelet = 'db10';
 name = 'filter_bank.txt';
+[Lo_D,Hi_D] = wfilters(wavelet,'d');
+S = [Lo_D Hi_D];
 file = fopen(name,'w');
 for i=1:length(S)
     fprintf(file,'%f\n',S(i));
